f453c8a3f734828177b77f2321e2658af3dc6efd
TheShadow88 <stef4oben88@gmail.com>
2019-11-16 10:14:08 +0200

    [Added] lexer and parser files added

--- Flex and bison calculator/Flex and bison calculator.cpp
+++ Flex and bison calculator/Flex and bison calculator.cpp
@@ -1,20 +1,24 @@
-// Flex and bison calculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+#include "parser.h"
+
+using namespace std;
+
+// this function is called syntax parser
+// just the parser, the parse
+extern int yyparse();
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	yyparse();
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// we have to code this function
+void yyerror(const char* msg)
+{
+	cout << "Error: " << msg << endl;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+}
